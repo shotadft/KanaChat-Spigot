8621dca2963568ed868f31337cf3f22576beca69
fubira <fubira@gmail.com>
2014-10-28 01:12:50 +0900

    Remove debug log

--- src/main/java/net/ironingot/nihongochat/NihongoChatAsyncPlayerChatListener.java
+++ src/main/java/net/ironingot/nihongochat/NihongoChatAsyncPlayerChatListener.java
@@ -2,7 +2,6 @@ package net.ironingot.nihongochat;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.logging.Logger;
 
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -16,13 +15,10 @@ import biscotte.kana.Kana;
 import net.ironingot.translator.KanaKanjiTranslator;
 
 public class NihongoChatAsyncPlayerChatListener implements Listener {
-    public static final Logger logger = Logger.getLogger("Minecraft");
-
     public NihongoChat plugin;
 
     private static final String avoidingString = 
             "[^\u0020-\u007E]|\u00a7|u00a74u00a75u00a73u00a74v|^http|^[A-Z#!<>]";
-
     private static final Pattern avoidingPattern = Pattern.compile(avoidingString);
 
     public NihongoChatAsyncPlayerChatListener(NihongoChat plugin) {
@@ -36,15 +32,11 @@ public class NihongoChatAsyncPlayerChatListener implements Listener {
         String message = event.getMessage();
         Player player = event.getPlayer();
 
-        logger.info("message: " + message);
-
         if (message.startsWith("/")) {
-            logger.info("start with /");
             return;
         }
 
         if (plugin.getConfigHandler().getUserMode(player.getName()).equals(Boolean.FALSE)) {
-            logger.info("Mode false");
             return;
         }
 
@@ -76,8 +68,6 @@ public class NihongoChatAsyncPlayerChatListener implements Listener {
             stringBuilder.append(ChatColor.GRAY).append(" ").append(message);
 
             event.setMessage(stringBuilder.toString());
-        } else {
-            logger.info("Matched.");
         }
     }
 }
