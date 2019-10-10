18980e3836d63d868c404faf1878a950646b1d43
fubira <fuibra@gmail.com>
2019-10-10 21:51:50 +0900

    先頭・末尾の記号処理

--- src/main/java/net/ironingot/kanachat/listener/AsyncPlayerChatListener.java
+++ src/main/java/net/ironingot/kanachat/listener/AsyncPlayerChatListener.java
@@ -21,11 +21,17 @@ public class AsyncPlayerChatListener implements Listener {
     private static final String excludeMatchString = "\u00a7|u00a74u00a75u00a73u00a74v|^http";
     private static final Pattern excludePattern = Pattern.compile(excludeMatchString);
 
-    private static final String prefixMatchString = "^(#GLOBAL#|>)(.*)";
+    private static final String systemMatchString = "^(#GLOBAL#|>)(.*)";
+    private static final Pattern systemPattern = Pattern.compile(systemMatchString);
+
+    private static final String wordMatchString = "([a-z0-9!-/:-@\\[-`\\{-~]*)";
+    private static final Pattern wordPattern = Pattern.compile(wordMatchString);
+
+    private static final String prefixMatchString = "^([0-9!-/:-@\\[-`\\{-~]+)(.*?)";
     private static final Pattern prefixPattern = Pattern.compile(prefixMatchString);
 
-    private static final String wordMatchString = "([a-z0-9 -@.,]*)";
-    private static final Pattern wordPattern = Pattern.compile(wordMatchString);
+    private static final String postfixMatchString = "(.*?)([0-9!-/:-@\\[-`\\{-~]+)$";
+    private static final Pattern postfixPattern = Pattern.compile(postfixMatchString);
 
     public AsyncPlayerChatListener(KanaChat plugin) {
         this.plugin = plugin;
@@ -33,17 +39,17 @@ public class AsyncPlayerChatListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
-        String prefix = "";
+        String system = "";
         String message = event.getMessage();
 
         if (message.startsWith("/")) {
             return;
         }
 
-        Matcher prefixMatcher = prefixPattern.matcher(message);
-        if (prefixMatcher.find(0)) {
-            prefix = prefixMatcher.group(1);
-            message = prefixMatcher.group(2);
+        Matcher systemMatcher = systemPattern.matcher(message);
+        if (systemMatcher.find(0)) {
+            system = systemMatcher.group(1);
+            message = systemMatcher.group(2);
         }
 
         Player player = event.getPlayer();
@@ -59,7 +65,7 @@ public class AsyncPlayerChatListener implements Listener {
         if (dstMessage.equals(message)) {
             return;
         }
-        event.setMessage(formatMessage(prefix, dstMessage, message));
+        event.setMessage(formatMessage(system, dstMessage, message));
     }
 
     private String formatMessage(String prefix, String dst, String src) {
@@ -96,6 +102,22 @@ public class AsyncPlayerChatListener implements Listener {
                 isLastTranslated = false;
                 continue;
             }
+
+            // find prefix signs
+            Matcher prefixMatcher = prefixPattern.matcher(word);
+            String prefix = "";
+            if (prefixMatcher.matches()) {
+                prefix = prefixMatcher.group(1);
+                word = prefixMatcher.group(2);
+            }
+        
+            // find postfix signs
+            Matcher postfixMatcher = postfixPattern.matcher(word);
+            String postfix = "";
+            if (postfixMatcher.matches()) {
+                word = postfixMatcher.group(1);
+                postfix = postfixMatcher.group(2);
+            }
         
             // Roma-Ji -> Hiragana translation
             Kana kana = new Kana();
@@ -130,7 +152,7 @@ public class AsyncPlayerChatListener implements Listener {
                 }
             }
 
-            stringBuilder.append(translatedWord);
+            stringBuilder.append(prefix + translatedWord + postfix);
         }
 
         return stringBuilder.toString();
