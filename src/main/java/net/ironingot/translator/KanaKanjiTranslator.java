3bcb111c15b33ba2fee55b7f8720fc1cdd3e4bba
shotadft <fukashta@gmail.com>
2025-03-17 00:44:05 +0900

    fix

--- src/main/java/net/ironingot/translator/KanaKanjiTranslator.java
+++ src/main/java/net/ironingot/translator/KanaKanjiTranslator.java
@@ -1,23 +1,22 @@
 package net.ironingot.translator;
 
 import org.apache.commons.collections4.map.PassiveExpiringMap;
-// import net.ironingot.kanachat.KanaChat;
+
+import java.util.concurrent.TimeUnit;
 
 public class KanaKanjiTranslator {
-    private static final long EXPIRE_AT = 1000 * 60 * 60 * 24 * 3; // 3days
+    private static final long EXPIRE_AT = TimeUnit.DAYS.toMillis(3); // 3days
 
     private static final PassiveExpiringMap<String, String> cache = new PassiveExpiringMap<>(EXPIRE_AT);
 
     public static String translate(String str) {
         String cached = cache.get(str);
         if (cached != null) {
-            // KanaChat.logger.info("KanaKanjiTranslator: Cache Hit [" + str + "] : [" + cached + "]");
             return cache.put(str, cached); // update expiring date
         }
 
         String result = GoogleTranslatorAPI.translate(str);
         cache.put(str, result);
-        // KanaChat.logger.info("KanaKanjiTranslator: Translate - [" + str + "] : [" + result + "]");
         return result;
     }
 }
